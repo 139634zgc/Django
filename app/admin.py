7f299ddfb866f63bdc46bb6484f49294f157f6c0
zhaogc <13001253655@163.com>
2018-02-24 15:04:06 +0800

    表单

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Moment
 
 # Register your models here.
+admin.site.register(Moment)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+from app.forms import MomentForm
+from django.http import  HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
 
+
+def moments_input(request):
+    if request.method == 'POST':
+        form = MomentForm(request.POST)
+        if form.is_valid():
+            moment = form.save()
+            moment.save()
+            return HttpResponseRedirect(reverse("app.views.welcome"))
+    else:
+        form = MomentForm()
+    PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    return render(request, os.path.join(PROJECT_ROOT, 'app/templates', 'moments_input.html'), {'form': form})
 
 def welcome(request):
     return HttpResponse("<h1>WELCOME TO MY SITE<h1>")
